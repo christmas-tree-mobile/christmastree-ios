f8fb42590c466cbe7e7c88fef5e28af89107c34b
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2017-11-18 19:02:41 +0000

    Added translations.

--- iOS/christmastree/translations/christmastree_de.qml.ts
+++ iOS/christmastree/translations/christmastree_de.qml.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../qml/Core/Dialog/HelpDialog.qml" line="109"/>
         <source>How to decorate Christmas Tree</source>
-        <translation type="unfinished"></translation>
+        <translation>Wie kann man einen Tannenbaum schmücken</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/HelpDialog.qml" line="110"/>
         <source>1. Press Settings button to choose how your tree and background image should look like. Press Settings button again to hide list of available options.</source>
-        <translation type="unfinished"></translation>
+        <translation>1. Drücken Sie die Schaltfläche Einstellungen, um auszuwählen, wie Ihr Baum und Ihr Hintergrundbild aussehen sollen. Drücken Sie erneut die Taste Einstellungen, um die Liste der verfügbaren Optionen auszublenden.</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/HelpDialog.qml" line="111"/>
         <source>2. Press Toys button to open toys gallery. Press Toys button again to hide this gallery.</source>
-        <translation type="unfinished"></translation>
+        <translation>2. Drücken Sie die Spielzeugtaste, um die Spielzeuggalerie zu öffnen. Drücken Sie die &quot;Spielzeug&quot; -Taste erneut, um diese Galerie auszublenden.</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/HelpDialog.qml" line="112"/>
         <source>3. Select toy you like from gallery by holding your finger on it and then drag selected toy to appropriate place on Christmas Tree. Press and hold your finger on the toy to send it to background or bring to foreground. Move toy away from Christmas Tree to remove it.</source>
-        <translation type="unfinished"></translation>
+        <translation>3. Wähle ein Spielzeug aus der Galerie aus, indem du deinen Finger darauf hältst und ziehe dann das ausgewählte Spielzeug an die entsprechende Stelle im Tannenbaum. Drücken und halten Sie Ihren Finger auf dem Spielzeug, um es in den Hintergrund zu bringen oder in den Vordergrund zu bringen. Bewegen Sie das Spielzeug weg vom Tannenbaum, um es zu entfernen.</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/HelpDialog.qml" line="113"/>
         <source>4. Repeat previous step, choosing different toys from gallery until you get your own wonderfully decorated Christmas Tree. Press Capture or Record buttons to save image or GIF of your Christmas Tree or share it with friends.</source>
-        <translation type="unfinished"></translation>
+        <translation>4. Wiederhole den vorherigen Schritt und wähle verschiedene Spielzeuge aus der Galerie, bis du deinen eigenen wunderschön geschmückten Weihnachtsbaum erhältst. Drücken Sie Photo oder Record, um Bild oder GIF Ihres Weihnachtsbaum zu speichern oder mit Freunden zu teilen.</translation>
     </message>
 </context>
 <context>
@@ -34,17 +34,17 @@
     <message>
         <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="172"/>
         <source>View ad and capture image</source>
-        <translation type="unfinished"></translation>
+        <translation>Anzeige ansehen und Bild erfassen</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="240"/>
         <source>Purchase full version</source>
-        <translation type="unfinished"></translation>
+        <translation>Vollversion kaufen</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="308"/>
         <source>Restore purchases</source>
-        <translation type="unfinished"></translation>
+        <translation>Käufe wiederherstellen</translation>
     </message>
 </context>
 </TS>
