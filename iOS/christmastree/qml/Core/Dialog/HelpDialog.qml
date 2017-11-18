d7c85eb7c1bc7ae9aa515d9af5ca014e709bd86e
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2017-11-18 23:47:40 +0000

    1. Updated translations; 2. Added scroll bar in help dialog.

--- iOS/christmastree/qml/Core/Dialog/HelpDialog.qml
+++ iOS/christmastree/qml/Core/Dialog/HelpDialog.qml
@@ -1,4 +1,5 @@
 import QtQuick 2.9
+import QtQuick.Controls 2.2
 
 MouseArea {
     id:               helpDialog
@@ -98,6 +99,10 @@ MouseArea {
             contentWidth:         helpText.width
             contentHeight:        helpText.height
             clip:                 true
+
+            ScrollBar.vertical: ScrollBar {
+                policy: ScrollBar.AlwaysOn
+            }
 
             Text {
                 id:                   helpText
--- iOS/christmastree/translations/christmastree_ru.ts
+++ iOS/christmastree/translations/christmastree_ru.ts
@@ -5,38 +5,38 @@
     <name>HelpDialog</name>
     <message>
         <source>How to decorate Christmas Tree</source>
-        <translation type="unfinished"></translation>
+        <translation>Как украсить новогоднюю елку</translation>
     </message>
     <message>
         <source>1. Press Settings button to choose how your tree and background image should look like. Press Settings button again to hide list of available options.</source>
-        <translation type="unfinished"></translation>
+        <translation>1. Нажмите кнопку Настройки, чтобы выбрать фоновое изображение и вид вашей елки. Намите кнопку Настройки еще раз, чтобы скрыть список доступных вариантов.</translation>
     </message>
     <message>
         <source>2. Press Toys button to open toys gallery. Press Toys button again to hide this gallery.</source>
-        <translation type="unfinished"></translation>
+        <translation>2. Нажмите кнопку Игрушки, чтобы открыть галерею елочных игрушек. Нажмите кнопку Игрушки еще раз, чтобы скрыть эту галерею.</translation>
     </message>
     <message>
         <source>3. Select toy you like from gallery by holding your finger on it and then drag selected toy to appropriate place on Christmas Tree. Press and hold your finger on the toy to send it to background or bring to foreground. Move toy away from Christmas Tree to remove it.</source>
-        <translation type="unfinished"></translation>
+        <translation>3. Выберите понравившуюся вам игрушку из галереи длительным нажатием пальца, а затем переместите ее на елку. Перемещайте игрушку за ветки или на передний план длительным нажатием пальца. Переместите игрушку за пределы елки для ее удаления с экрана.</translation>
     </message>
     <message>
         <source>4. Repeat previous step, choosing different toys from gallery until you get your own wonderfully decorated Christmas Tree. Press Capture or Record buttons to save image or GIF of your Christmas Tree or share it with friends.</source>
-        <translation type="unfinished"></translation>
+        <translation>4. Повторяйте предыдущий шаг, выбирая различные игрушки из галереи. После того, как елка будет полностью украшена, нажмите кнопки Снимок или Запись для того, чтобы сохранить изображение или GIF-ролик вашей елки, или поделиться ими с друзьями.</translation>
     </message>
 </context>
 <context>
     <name>PurchaseDialog</name>
     <message>
         <source>View ad and capture image</source>
-        <translation type="unfinished"></translation>
+        <translation>Посмотреть рекламу и сохранить снимок</translation>
     </message>
     <message>
         <source>Purchase full version</source>
-        <translation type="unfinished"></translation>
+        <translation>Купить полную версию</translation>
     </message>
     <message>
         <source>Restore purchases</source>
-        <translation type="unfinished"></translation>
+        <translation>Восстановить покупки</translation>
     </message>
 </context>
 </TS>
