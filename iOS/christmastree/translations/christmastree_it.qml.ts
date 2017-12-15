592d2485ac2edce00fa491c6fdd72dcf4218c5c2
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2017-12-15 21:13:26 +0000

    Added Spanish and Italian translations.

--- iOS/christmastree/translations/christmastree_it.qml.ts
+++ iOS/christmastree/translations/christmastree_it.qml.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../qml/Core/Dialog/HelpDialog.qml" line="114"/>
         <source>How to decorate Christmas Tree</source>
-        <translation type="unfinished"></translation>
+        <translation>Come decorare l&apos;albero di Natale</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/HelpDialog.qml" line="115"/>
         <source>1. Press Settings button to choose how your tree and background image should look like. Press Settings button again to hide list of available options.</source>
-        <translation type="unfinished"></translation>
+        <translation>1. Premere il pulsante Impostazioni per scegliere come dovrebbe apparire l&apos;albero e l&apos;immagine di sfondo. Premere nuovamente il pulsante Impostazioni per nascondere l&apos;elenco delle opzioni disponibili.</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/HelpDialog.qml" line="116"/>
         <source>2. Press Toys button to open toys gallery. Press Toys button again to hide this gallery.</source>
-        <translation type="unfinished"></translation>
+        <translation>2. Premi il pulsante Toys per aprire la galleria di giocattoli. Premi ancora il pulsante Toys per nascondere questa galleria.</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/HelpDialog.qml" line="117"/>
         <source>3. Select toy you like from gallery by holding your finger on it and then drag selected toy to appropriate place on Christmas Tree. Press and hold your finger on the toy to send it to background or bring to foreground. Move toy away from Christmas Tree to remove it.</source>
-        <translation type="unfinished"></translation>
+        <translation>3. Seleziona il giocattolo che ti piace dalla galleria tenendo il dito su di esso e trascina il giocattolo selezionato nel punto appropriato sull&apos;albero di Natale. Premi e tieni premuto il dito sul giocattolo per inviarlo allo sfondo o portare in primo piano. Sposta il giocattolo lontano dall&apos;albero di Natale per rimuoverlo.</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/HelpDialog.qml" line="118"/>
         <source>4. Repeat previous step, choosing different toys from gallery until you get your own wonderfully decorated Christmas Tree. Press Capture or Record buttons to save image or GIF of your Christmas Tree or share it with friends.</source>
-        <translation type="unfinished"></translation>
+        <translation>4. Ripeti il passaggio precedente, scegliendo diversi giocattoli dalla galleria fino ad ottenere il tuo albero di Natale meravigliosamente decorato. Premi i pulsanti Capture o Record per salvare l&apos;immagine o la GIF del tuo albero di Natale o condividerla con gli amici.</translation>
     </message>
 </context>
 <context>
@@ -34,7 +34,7 @@
     <message>
         <location filename="../qml/Core/Dialog/ParentalGateDialog.qml" line="131"/>
         <source>Slide with two fingers over this dialog to continue</source>
-        <translation type="unfinished"></translation>
+        <translation>Scorri con due dita su questa finestra di dialogo per continuare</translation>
     </message>
 </context>
 <context>
@@ -42,17 +42,17 @@
     <message>
         <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="172"/>
         <source>View ad and capture image</source>
-        <translation type="unfinished"></translation>
+        <translation>Visualizza annunci e acquisisci immagini</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="240"/>
         <source>Purchase full version</source>
-        <translation type="unfinished"></translation>
+        <translation>Acquista la versione completa</translation>
     </message>
     <message>
         <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="308"/>
         <source>Restore purchases</source>
-        <translation type="unfinished"></translation>
+        <translation>Ripristinare gli acquisti</translation>
     </message>
 </context>
 </TS>
