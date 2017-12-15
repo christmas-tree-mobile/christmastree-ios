527032e5beefcfa660d23246d6968b66a78db14f
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2017-12-15 21:16:12 +0000

    Updated translations.

--- iOS/christmastree/translations/christmastree_es.ts
+++ iOS/christmastree/translations/christmastree_es.ts
@@ -5,45 +5,45 @@
     <name>HelpDialog</name>
     <message>
         <source>How to decorate Christmas Tree</source>
-        <translation type="unfinished"></translation>
+        <translation>Cómo decorar el árbol de Navidad</translation>
     </message>
     <message>
         <source>1. Press Settings button to choose how your tree and background image should look like. Press Settings button again to hide list of available options.</source>
-        <translation type="unfinished"></translation>
+        <translation>1. Presione el botón Configuración para elegir cómo debe verse su árbol y su imagen de fondo. Presione el botón Configuración nuevamente para ocultar la lista de opciones disponibles.</translation>
     </message>
     <message>
         <source>2. Press Toys button to open toys gallery. Press Toys button again to hide this gallery.</source>
-        <translation type="unfinished"></translation>
+        <translation>2. Presione el botón de Juguetes para abrir la galería de juguetes. Presione el botón de Toys nuevamente para ocultar esta galería.</translation>
     </message>
     <message>
         <source>3. Select toy you like from gallery by holding your finger on it and then drag selected toy to appropriate place on Christmas Tree. Press and hold your finger on the toy to send it to background or bring to foreground. Move toy away from Christmas Tree to remove it.</source>
-        <translation type="unfinished"></translation>
+        <translation>3. Selecciona el juguete que te gusta de la galería manteniendo tu dedo sobre él y luego arrastra el juguete seleccionado al lugar apropiado en el Árbol de Navidad. Mantenga presionado el dedo sobre el juguete para enviarlo al fondo o ponerlo en primer plano. Mueva el juguete fuera del árbol de Navidad para quitarlo.</translation>
     </message>
     <message>
         <source>4. Repeat previous step, choosing different toys from gallery until you get your own wonderfully decorated Christmas Tree. Press Capture or Record buttons to save image or GIF of your Christmas Tree or share it with friends.</source>
-        <translation type="unfinished"></translation>
+        <translation>4. Repita el paso anterior, eligiendo diferentes juguetes de la galería hasta que obtenga su propio árbol de Navidad maravillosamente decorado. Presione los botones Capturar o Grabar para guardar la imagen o el GIF de su Árbol de Navidad o compartirlo con amigos.</translation>
     </message>
 </context>
 <context>
     <name>ParentalGateDialog</name>
     <message>
         <source>Slide with two fingers over this dialog to continue</source>
-        <translation type="unfinished"></translation>
+        <translation>Deslice con dos dedos sobre este diálogo para continuar</translation>
     </message>
 </context>
 <context>
     <name>PurchaseDialog</name>
     <message>
         <source>View ad and capture image</source>
-        <translation type="unfinished"></translation>
+        <translation>Ver anuncio y captura de imagen</translation>
     </message>
     <message>
         <source>Purchase full version</source>
-        <translation type="unfinished"></translation>
+        <translation>Comprar la versión completa</translation>
     </message>
     <message>
         <source>Restore purchases</source>
-        <translation type="unfinished"></translation>
+        <translation>Restaurar las compras</translation>
     </message>
 </context>
 </TS>
