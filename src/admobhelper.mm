347b2c99438a51be12468bbe124993213a94ada1
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-06-09 23:02:33 +0300

    Style nits.

--- src/admobhelper.mm
+++ src/admobhelper.mm
@@ -31,7 +31,7 @@ const QString AdMobHelper::ADMOB_TEST_DEVICE_ID      ("");
 {
     self = [super init];
 
-    if (self) {
+    if (self != nil) {
         AdMobHelperInstance = helper;
 
         UIViewController * __block root_view_controller = nil;
@@ -158,7 +158,7 @@ const QString AdMobHelper::ADMOB_TEST_DEVICE_ID      ("");
 {
     self = [super init];
 
-    if (self) {
+    if (self != nil) {
         Interstitial        = nil;
         AdMobHelperInstance = helper;
     }
