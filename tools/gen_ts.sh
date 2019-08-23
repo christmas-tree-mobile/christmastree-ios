dbc24acce4698ccab6cb6db1bbadd01379e18f5f
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-08-23 04:19:13 +0300

    Update scripts for Qt 5.13.0.

--- tools/gen_ts.sh
+++ tools/gen_ts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PATH=$PATH:~/Qt/5.12.1/ios/bin
+PATH=$PATH:~/Qt/5.13.0/ios/bin
 
 lupdate ../christmastree.pro -ts ../translations/christmastree_ru.src.ts
 lupdate ../qml               -ts ../translations/christmastree_ru.qml.ts
