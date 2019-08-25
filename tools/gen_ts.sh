44949e9cf3bb042c3fbbcb4ded5f78337a2b8326
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-08-25 19:46:17 +0300

    Always add locations to .ts files.

--- tools/gen_ts.sh
+++ tools/gen_ts.sh
@@ -2,23 +2,23 @@
 
 PATH=$PATH:~/Qt/5.13.0/ios/bin
 
-lupdate ../christmastree.pro -ts ../translations/christmastree_ru.src.ts
-lupdate ../qml               -ts ../translations/christmastree_ru.qml.ts
+lupdate -locations absolute ../christmastree.pro -ts ../translations/christmastree_ru.src.ts
+lupdate -locations absolute ../qml               -ts ../translations/christmastree_ru.qml.ts
 
-lupdate ../christmastree.pro -ts ../translations/christmastree_de.src.ts
-lupdate ../qml               -ts ../translations/christmastree_de.qml.ts
+lupdate -locations absolute ../christmastree.pro -ts ../translations/christmastree_de.src.ts
+lupdate -locations absolute ../qml               -ts ../translations/christmastree_de.qml.ts
 
-lupdate ../christmastree.pro -ts ../translations/christmastree_fr.src.ts
-lupdate ../qml               -ts ../translations/christmastree_fr.qml.ts
+lupdate -locations absolute ../christmastree.pro -ts ../translations/christmastree_fr.src.ts
+lupdate -locations absolute ../qml               -ts ../translations/christmastree_fr.qml.ts
 
-lupdate ../christmastree.pro -ts ../translations/christmastree_zh.src.ts
-lupdate ../qml               -ts ../translations/christmastree_zh.qml.ts
+lupdate -locations absolute ../christmastree.pro -ts ../translations/christmastree_zh.src.ts
+lupdate -locations absolute ../qml               -ts ../translations/christmastree_zh.qml.ts
 
-lupdate ../christmastree.pro -ts ../translations/christmastree_es.src.ts
-lupdate ../qml               -ts ../translations/christmastree_es.qml.ts
+lupdate -locations absolute ../christmastree.pro -ts ../translations/christmastree_es.src.ts
+lupdate -locations absolute ../qml               -ts ../translations/christmastree_es.qml.ts
 
-lupdate ../christmastree.pro -ts ../translations/christmastree_it.src.ts
-lupdate ../qml               -ts ../translations/christmastree_it.qml.ts
+lupdate -locations absolute ../christmastree.pro -ts ../translations/christmastree_it.src.ts
+lupdate -locations absolute ../qml               -ts ../translations/christmastree_it.qml.ts
 
 lconvert ../translations/christmastree_ru.src.ts ../translations/christmastree_ru.qml.ts -o ../translations/christmastree_ru.ts
 lconvert ../translations/christmastree_de.src.ts ../translations/christmastree_de.qml.ts -o ../translations/christmastree_de.ts
