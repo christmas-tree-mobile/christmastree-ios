e6d01541177ce9620b33f0f556ebd9819e4daf00
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-06-25 16:30:44 +0300

    Fix namespace formatting.

--- src/admobhelper.mm
+++ src/admobhelper.mm
@@ -12,7 +12,9 @@ const QString AdMobHelper::ADMOB_INTERSTITIAL_UNIT_ID(QStringLiteral("ca-app-pub
 const QString AdMobHelper::ADMOB_TEST_DEVICE_ID      (QStringLiteral(""));
 
 namespace {
-    constexpr NSTimeInterval AD_RELOAD_ON_FAILURE_DELAY = 60.0;
+
+constexpr NSTimeInterval AD_RELOAD_ON_FAILURE_DELAY = 60.0;
+
 }
 
 @interface BannerViewDelegate : NSObject<GADBannerViewDelegate>
