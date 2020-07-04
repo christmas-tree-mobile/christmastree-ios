2d7bfe6ca1ebd36d0ced651d4f86128eb544c07f
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-07-05 02:13:39 +0300

    Update scripts for Qt 5.12.9.

--- tools/gen_ts.sh
+++ tools/gen_ts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PATH=$PATH:~/Qt/5.13.0/ios/bin
+PATH=$PATH:~/Qt/5.12.9/ios/bin
 
 lupdate -locations absolute ../christmastree.pro -ts ../translations/christmastree_de.src.ts
 lupdate -locations absolute ../qml               -ts ../translations/christmastree_de.qml.ts
