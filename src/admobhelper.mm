9195789d5186b80ec0f38feef24802ed3c1cea80
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-07-16 23:32:14 +0300

    Recreate intersitial in interstitialDidDismissScreen instead of loadAd.

--- src/admobhelper.mm
+++ src/admobhelper.mm
@@ -187,8 +187,11 @@ constexpr NSTimeInterval AD_RELOAD_ON_FAILURE_DELAY = 60.0;
 
     if (self != nil) {
         ShowPersonalizedAds = NO;
-        Interstitial        = nil;
         AdMobHelperInstance = helper;
+
+        Interstitial = [[GADInterstitial alloc] initWithAdUnitID:AdMobHelper::ADMOB_INTERSTITIAL_UNIT_ID.toNSString()];
+
+        Interstitial.delegate = self;
     }
 
     return self;
@@ -219,14 +222,6 @@ constexpr NSTimeInterval AD_RELOAD_ON_FAILURE_DELAY = 60.0;
 
 - (void)loadAd
 {
-    Interstitial.delegate = nil;
-
-    [Interstitial release];
-
-    Interstitial = [[GADInterstitial alloc] initWithAdUnitID:AdMobHelper::ADMOB_INTERSTITIAL_UNIT_ID.toNSString()];
-
-    Interstitial.delegate = self;
-
     GADRequest *request = [GADRequest request];
 
     if (!ShowPersonalizedAds) {
@@ -287,9 +282,17 @@ constexpr NSTimeInterval AD_RELOAD_ON_FAILURE_DELAY = 60.0;
 {
     Q_UNUSED(ad)
 
+    Interstitial.delegate = nil;
+
+    [Interstitial release];
+
     if (AdMobHelperInstance != nullptr) {
         AdMobHelperInstance->SetInterstitialActive(false);
     }
+
+    Interstitial = [[GADInterstitial alloc] initWithAdUnitID:AdMobHelper::ADMOB_INTERSTITIAL_UNIT_ID.toNSString()];
+
+    Interstitial.delegate = self;
 
     [self performSelector:@selector(loadAd) withObject:nil afterDelay:0.0];
 }
